4115db3330c1ca66143dd549791e5e3640d8c5e4
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 17:11:21 +0800

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,5 @@
 class AppConstants {
-  // 默认TVBox配置源（你的乱码解码后的URL）
-  static const String defaultSourceUrl = 
-      'https://gh-proxy.com/https://raw.githubusercontent.com/fantaiying7/EXT/refs/heads/main/drpy2.min.js'; // 稍后教你解码
-
-  // 解析接口备用
+  // 默认解析接口
   static const List<String> parseInterfaces = [
     "https://bd.jx.cn/?url=",
     "https://www.8090g.cn/?url=",
@@ -11,4 +7,8 @@ class AppConstants {
   ];
 
   static const List<String> interfaceNames = ["冰豆", "8090", "云析"];
+
+  // 存储键
+  static const String keySourceUrl = 'tvbox_source_url';
+  static const String keySourceContent = 'tvbox_source_content';
 }
