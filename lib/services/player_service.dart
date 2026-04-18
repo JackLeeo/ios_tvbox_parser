bc82af34eb3569912380e83064d04702721e24e7
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 17:13:54 +0800

    Update player_service.dart

--- lib/services/player_service.dart
+++ lib/services/player_service.dart
@@ -1,7 +1,6 @@
 import '../utils/constants.dart';
 
 class PlayerService {
-  /// 根据视频播放页 URL 生成解析后的播放地址
   static String getParseUrl(String videoUrl, {int parserIndex = 0}) {
     final base = AppConstants.parseInterfaces[parserIndex];
     return '$base${Uri.encodeComponent(videoUrl)}';
