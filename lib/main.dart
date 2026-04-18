e16767410529409dfd389beb9dc3f855649a86b1
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 17:16:28 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
 import 'pages/settings_page.dart';
 import 'services/storage_service.dart';
-import 'services/config_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,49 +19,8 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
         appBarTheme: const AppBarTheme(backgroundColor: Colors.black, elevation: 0),
       ),
-      home: const SplashPage(), // 启动页决定跳转
+      home: const SettingsPage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
-
-  @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    _checkConfig();
-  }
-
-  Future<void> _checkConfig() async {
-    final config = await ConfigService().loadConfig();
-    if (mounted) {
-      if (config != null) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const HomePage()),
-        );
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const SettingsPage()),
-        );
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
     );
   }
 }
