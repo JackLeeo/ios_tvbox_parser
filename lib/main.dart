260f0ce465232cd9e183900a52eb8af2addef75c
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 15:52:16 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '视频嗅探播放器',
+      title: '原生视频解析',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.black,
