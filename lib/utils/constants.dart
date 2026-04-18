9310d4c6b7b28d48173ba97c9b59de890be6ad39
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 16:09:20 +0800

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,4 @@
 class AppConstants {
-  // 解析接口列表
   static const List<String> parseInterfaces = [
     "https://bd.jx.cn/?url=",
     "https://api.qianqi.net/vip/?url=",
@@ -8,6 +7,4 @@ class AppConstants {
   static const List<String> interfaceNames = [
     "冰豆","千奇"
   ];
-
-  static const String userAgent = "Mozilla/5.0";
 }
