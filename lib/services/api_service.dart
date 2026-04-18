6f985abf90c626203768112e01a67dae73a298b5
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-18 17:02:02 +0800

    Create api_service.dart

---
+++ lib/services/api_service.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import '../models/video.dart';
+import '../models/source.dart';
+
+class ApiService {
+  final Dio _dio = Dio();
+
+  /// 获取首页推荐（以第一个站点为例）
+  Future<List<VideoItem>> getHomeList(SiteRule site, {int page = 1}) async {
+    try {
+      final url = '${site.api}?ac=videolist&pg=$page';
+      final response = await _dio.get(url, options: Options(headers: site.headers));
+      if (response.data['code'] == 1) {
+        final list = response.data['list'] as List;
+        return list.map((e) => VideoItem.fromJson(e)).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  /// 搜索
+  Future<List<VideoItem>> search(SiteRule site, String keyword) async {
+    try {
+      final url = site.searchUrl ?? '${site.api}?ac=videolist&wd=$keyword';
+      final response = await _dio.get(url, options: Options(headers: site.headers));
+      if (response.data['code'] == 1) {
+        final list = response.data['list'] as List;
+        return list.map((e) => VideoItem.fromJson(e)).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  /// 获取剧集
+  Future<List<Episode>> getEpisodes(SiteRule site, String videoId) async {
+    try {
+      final url = '${site.api}?ac=videolist&ids=$videoId';
+      final response = await _dio.get(url, options: Options(headers: site.headers));
+      if (response.data['code'] == 1 && response.data['list'].isNotEmpty) {
+        final vod = response.data['list'][0];
+        final playList = vod['vod_play_list'] as Map<String, dynamic>;
+        // 通常格式：{"播放源1":"第1集$url#第2集$url", ...}
+        final List<Episode> episodes = [];
+        playList.forEach((source, urls) {
+          final parts = urls.split('#');
+          for (var part in parts) {
+            final pair = part.split('\$');
+            if (pair.length == 2) {
+              episodes.add(Episode(name: pair[0], url: pair[1]));
+            }
+          }
+        });
+        return episodes;
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+}
